99a96f11df33f0a52af4e0248de8eb5fda7ed6bd
satelite54 <taehune94@gmail.com>
2020-12-07 22:53:35 +0900

    - jfoenix, ojbc6_g 라이브러리 프로젝트 포함 - CPage 클래스 추가 - View model img 패키지 추가 - 9번 페이지 초기 세팅

---
+++ PROJECT1/.metadata/.plugins/org.eclipse.core.resources/.history/86/608539aa8e38001b14dc99d249de5832
@@ -0,0 +1,16 @@
+package model;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class CPage {
+	public void CreatePage(Stage stage, String FXMLRout) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource(FXMLRout));
+		stage.setScene(new Scene(root));
+		stage.show();
+	}
+}
--- PROJECT1/Pharmacy_management_system/src/application/Main.java
+++ PROJECT1/Pharmacy_management_system/src/application/Main.java
@@ -2,16 +2,18 @@ package application;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-
+import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			Parent root = fxmlLoader.load(getClass().getResource("../view/Main.fxml"));
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
