b102fa6d0f9c08c9870f5bb2e4bcef4ad0d481d0
Ahyoung <ahyoungpark93@gmail.com>
2020-12-08 10:44:13 +0900

    원래 있던 프로젝트 제거

--- PROJECT1/MedApp/src/application/Main.java
+++ PROJECT1/MedApp/src/application/Main.java
@@ -11,9 +11,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader fxmlLoader = new FXMLLoader();
-			Parent root = fxmlLoader.load(getClass().getResource("../view/Main.fxml"));
-			Scene scene = new Scene(root);
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,1248,1024);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
