19cc80518f646d3e2a8ac2655760c7bf41c464fe
Ahyoung <ahyoungpark93@gmail.com>
2020-12-08 12:58:41 +0900

    제출과제파일 올립니다.

---
+++ 1조DB.sql
@@ -0,0 +1,23 @@
+-- User Table
+CREATE TABLE User
+(
+    name        VARCHAR2(10)    NOT NULL, 
+    id          VARCHAR2(10)    NULL, 
+    password    VARCHAR2(4)     NULL, 
+    permit      VARCHAR2(20)    NULL, 
+    CONSTRAINT USER_PK PRIMARY KEY (id)
+)
+
+-- Medicine Table
+CREATE TABLE Medicine
+(
+    name          VARCHAR2(20)    NULL, 
+    character     VARCHAR2(20)    NULL, 
+    effect        VARCHAR2(20)    NULL, 
+    warning       VARCHAR2(20)    NULL, 
+    company       VARCHAR2        NULL, 
+    expiration    DATE            NULL, 
+    price         NUMBER          NULL, 
+    CONSTRAINT MEDICINE_PK PRIMARY KEY (name)
+)
+
