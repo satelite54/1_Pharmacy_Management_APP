f98ddf777a1205dc871bee5e53b7a7fe30dab832
satelite54 <taehune94@gmail.com>
2020-12-08 11:21:48 +0900

    [TEST]  - 새 프로젝트에서 실행 잘되는지 확인

--- MedApp/src/Kimtaeheon/Main_FOR_TEST_KIMTAEHEON.java
+++ MedApp/src/Kimtaeheon/Main_FOR_TEST_KIMTAEHEON.java
@@ -1,18 +1,18 @@
-package application;
+package Kimtaeheon;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
 
-
-public class Main extends Application {
+public class Main_FOR_TEST_KIMTAEHEON extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("../view/Main_Kimtaeheon.fxml"));
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			Parent root = fxmlLoader.load(getClass().getResource("../view/Main_KimtaeHeon.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
