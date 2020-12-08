5306aff68a2b989b697937a105493c3d78759066
Ahyoung <ahyoungpark93@gmail.com>
2020-12-08 10:31:45 +0900

    no message

--- PROJECT1/Pharmacy_management_system/src/application/Main.java
+++ PROJECT1/Pharmacy_management_system/src/application/Main.java
@@ -4,15 +4,14 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			FXMLLoader fxmlLoader = new FXMLLoader();
-			Parent root = fxmlLoader.load(getClass().getResource("../view/Main.fxml"));
+			
+			Parent root = FXMLLoader.load(getClass().getResource("../view/DBRegister.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
--- PROJECT1/Pharmacy_management_system/src/view/DBRegisterController.java
+++ PROJECT1/Pharmacy_management_system/src/view/DBRegisterController.java
@@ -1,7 +1,11 @@
 package view;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
 public class DBRegisterController {
+	
+	@FXML
+	AnchorPane anPane;
 
 }
