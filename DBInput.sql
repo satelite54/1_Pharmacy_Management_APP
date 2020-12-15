6f15990ffafe34585daa9fb681bcc22f499adcd8
DESKTOP-PC86ECB\curong <curong@outlook.kr>
2020-12-15 17:37:51 +0900

    DBinput (마스크관련 추가)

--- DBInput.sql
+++ DBInput.sql
@@ -46,7 +46,50 @@ VALUES
    ("레모나산", "file:/C:/MedApp/src/img/lemona.jpg", "황색의 미립상 산제", "B2, B6, C의 보급, 기미, 주근깨 완화", "1일 1회 1포", "경남제약주식회사", ('2021/01/20', 'YYYY/MM/DD'), 18000, 14),
    ;
    
-   
+   CREATE TABLE Maskstock
+(
+    KFType      VARCHAR2(20)    NOT NULL, 
+    stock       NUMBER          NULL, 
+    CONSTRAINT MASK_PK PRIMARY KEY (KFType)
+);
+
+INSERT INTO maskstock (KFType, stock) VALUES ('KF-94',320);
+INSERT INTO maskstock (KFType, stock) VALUES ('KF-90',282);
+INSERT INTO maskstock (KFType, stock) VALUES ('KF-80',343);
+INSERT INTO maskstock (KFType, stock) VALUES ('KF-AD',362);
+
+CREATE TABLE BuyingMask
+(
+    KFType2      VARCHAR2(20)    NOT NULL,
+    RRnumber    VARCHAR2(13)    NOT NULL,
+    available   NUMBER          NOT NULL
+    );
+
+
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-94', '9412251234567', 2);
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-90', '9412251234567', 2);
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-80', '9412251234567', 2);
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-AD', '9412251234567', 2);
+
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-94', '9312251234567', 1);
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-90', '9312251234567', 1);
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-80', '9312251234567', 1);
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-AD', '9312251234567', 1);
+
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-94', '9412251234567', 0);
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-90', '9412251234567', 0);
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-80', '9412251234567', 0);
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-AD', '9412251234567', 0);
+
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-94', '9612251234567', 3);
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-90', '9612251234567', 3);
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-80', '9612251234567', 3);
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-AD', '9612251234567', 3);
+
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-94', '9712251234567', 1);
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-90', '9712251234567', 1);
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-80', '9712251234567', 1);
+INSERT INTO BuyingMask (KFType2, RRnumber, available) VALUES ('KF-AD', '9712251234567', 1);
    
    
    
